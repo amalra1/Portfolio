5011d3db6a53ecc7c2aa763cfb7ad6e337c8347c
Pedro Amaral <amaral1@ufpr.br>
2025-07-25 20:42:12 -0300

    feat: Added new badge

--- src/components/skillCard/SkillCard.tsx
+++ src/components/skillCard/SkillCard.tsx
@@ -4,15 +4,17 @@ import styles from './SkillCard.module.css';
 import Icon from '@/components/icon/Icon';
 
 import awsCloudPractitioner from '@/assets/badges/aws-cloud-quest-cloud-practitioner.png';
-import awsGenAI from '@/assets/badges/aws-cloud-quest-generative-ai-practitioner.png';
+//import awsGenAI from '@/assets/badges/aws-cloud-quest-generative-ai-practitioner.png';
 import awsKnowledge from '@/assets/badges/aws-knowledge-cloud-essentials.png';
 import awsPartner from '@/assets/badges/aws-partner-generative-ai-essentials.png';
+import awsGenAIArchitect from '@/assets/badges/aws-cloud-quest-generative-ai-architect.png';
 
 const badgeMap: { [key: string]: StaticImageData } = {
   'aws-cloud-quest-cloud-practitioner': awsCloudPractitioner,
-  'aws-cloud-quest-generative-ai-practitioner': awsGenAI,
+  //'aws-cloud-quest-generative-ai-practitioner': awsGenAI,
   'aws-knowledge-cloud-essentials': awsKnowledge,
   'aws-partner-generative-ai-essentials': awsPartner,
+  'aws-cloud-quest-generative-ai-architect': awsGenAIArchitect,
 };
 
 type Skill = {
